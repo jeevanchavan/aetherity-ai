de35d2b2e86645be485b5438931b3f3ee34ae318
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-06-15 01:03:14 +0530

    latest commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 app.use(morgan("dev"))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"process.env.APP_URL",
     credentials:true
 }))
 
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -32,7 +32,7 @@ export async function registerUser(req, res) {
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="${process.env.APP_URL}/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
@@ -162,7 +162,7 @@ export async function verifyEmail(req,res){
             `
             <h1>Email Verified Successfully!</h1>
             <p>Your email has been verified. You can now log in to your account.</p>
-            <a href="http://localhost:5173/login">Go to Login</a>
+            <a href="${process.env.APP_URL}/login">Go to Login</a>
         `
 
         return res.send(html)
