5df74c4106d6799f9e4885b82a2f60109c2691ea
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-06-15 00:44:05 +0530

    deployment commits

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,16 @@ import express from 'express'
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// It creates __dirname manually in ES modules.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
+app.use(express.static("./public"));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
@@ -20,5 +27,10 @@ import authRouter from './routes/auth.routes.js';
 import chatRouter from './routes/chat.routes.js';
 app.use("/api/auth",authRouter)
 app.use("/api/chats",chatRouter);
+
+// wildcard api
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
+})
 
 export default app;
